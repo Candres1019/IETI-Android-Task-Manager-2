e105ff2b5be4c7c208d13d7c7c5d2f29f8e05e68
Andrés Calderón <mateo.cor.amco@gmail.com>
2021-11-27 19:24:26 -0500

    Initial Commit

--- app/src/main/java/com/example/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/taskmanager/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.taskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(view -> {
+            TextView textView = findViewById(R.id.textView);
+            textView.setText("Bienvenidos a la parte de android");
+        });
     }
 }
